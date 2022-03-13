f5ed36f2955e3d6633ecc9bc9774cb17cd6e6e90
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2022-03-13 22:02:47 +0530

    Update sde_1.py

--- sde_1.py
+++ sde_1.py
@@ -83,23 +83,7 @@ class pet_pooja_app:
    btn_close.bind('<Enter>', btn_e)
    btn_close.bind('<Leave>', btn_l)
    btn_close.bind('<Button-1>', btn_clk)
-   """
-   dat1 = getmenus()
-   dat2 = getorders()
-   x1=0
-   y1=0
-   z1=0
-   for xx in dat2:
-    for yy in xx:
-      lab_o = Label(ocan1, text=yy, bg='#13700c', fg='#ffffff', font=('Arial', 8))
-      lab_o.place(x=10+x1, y=30+y1)
-      x1 = x1+60
-   y1=y1+20
-  for zz in dat1:
-     lab_f = Label(fcan1, text=zz, bg='#b51908', fg='#ffffff', font=('Arial', 8))
-     lab_f.place(x=10, y=30+z1)
-     z1=z1+20
-  """
+   #paste code here
  def init_ui():
    tt = Tk()
    tt.geometry(('%dx%d')%(tt.winfo_screenwidth(), tt.winfo_screenheight()))
